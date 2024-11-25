ef5bd227e6e822542bd7c5cc89b843f115d32fec
João Freitas <a25077@esenviseu.net>
2024-11-25 15:59:35 +0000

    relogio

--- relogio.py
+++ relogio.py
@@ -1,9 +1,12 @@
 from datetime import datetime
 import time
-
+despertou = False
 while True:
     print(datetime.now())
     hora_atual = datetime.now().strftime("%H:%M:%S")
     print(hora_atual)
     #esperar 1 segundo
     time.sleep(1)
+    if datetime.now().hour >= 16 and datetime.now().minute >= 35 and datetime.now().second >= 0 and despertou == False:
+        print("ACORDA!!!!!!")
+        despertou = True

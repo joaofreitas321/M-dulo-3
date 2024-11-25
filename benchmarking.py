e47663f5e2bcc8d73c277a55cc82abbea2b8cac3
João Freitas <a25077@esenviseu.net>
2024-11-25 16:18:28 +0000

    bench marking

---
+++ benchmarking.py
@@ -0,0 +1,20 @@
+import math
+from datetime import datetime
+
+#calcula a exponenciação utilizando o operador **
+def funcao_complicada():
+    for i in range(1000000):
+        _ = i ** 2
+#calcula a exponenciação utilizando a função pow do módulo math
+def funcao_complicada2():
+    for i in range(1000000):
+        _ = math.pow(i,2)
+
+def medir_tempo():
+    inicio = datetime.now()
+    funcao_complicada()
+    fim = datetime.now()
+    tempo_execucao = fim -inicio
+    print("Tempo de execução:",tempo_execucao.total_seconds())
+
+medir_tempo()    

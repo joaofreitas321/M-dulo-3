a2ffd1f4f90c4b756835db424b4b37b2179ca2d6
João Freitas <a25077@esenviseu.net>
2024-12-04 10:49:18 +0000

    restaurante

--- restaurante.py
+++ restaurante.py
@@ -47,31 +47,42 @@ def SaidaMesas(n_atual_mesas):
         return 0
     n_mesas = utils.ler_numero_inteiro_limites(1,n_atual_mesas,"Quantas mesas ficaram desocupadas:")
     return n_mesas
-def Estado(max_mesas,max_clientes,mesas,clientes):
+def Estado(max_mesas,max_clientes,mesas,clientes,total):
     """
     Função que calcula e mostra os dados estatísticos do estado do restaurante
     """
-    print(clientes)
+    print(f"Tem {mesas} mesas ocupadas e {max_mesas - mesas} mesas livres")
+    print(f"Tem {clientes} clientes no restaurante")
+    print(f"Que corresponde a uma ocupação de {clientes/max_clientes*100}%")
+    print(f"Já recebeu {total}€ das refeições servidas")
+
 def Menu():
     n_max_mesas = utils.ler_numero_inteiro("Quantas mesas tem o restaurante:")
     n_max_clientes = utils.ler_numero_inteiro("Quantos clientes o restaurante pode ter:")
     op = 1
     n_atual_mesas = 0
     n_atual_clientes = 0
+    total_pago = 0
     while op != 4:
         op = utils.ler_numero_inteiro_limites(1,4,"1.Entrada\2.Saída\3.Estado\4.Terminar")
         if op == 1:
+            # Entrada dos clientes
             n_clientes_entraram = EntradaClientes(n_max_clientes,n_atual_clientes)
             n_mesas_ocupadas = EntradaMesas(n_max_mesas,n_atual_mesas)
             n_atual_clientes += n_clientes_entraram
             n_atual_mesas += n_mesas_ocupadas
         if op == 2:
+            # Saída dos clientes
             n_clientes_sairam = SaidaClientes(n_atual_clientes)
             n_mesas_desocupadas = SaidaMesas(n_atual_mesas)
             n_atual_clientes = n_atual_clientes - n_clientes_sairam
             n_atual_mesas = n_atual_mesas - n_mesas_desocupadas
+            # Para evitar perguntar o custo da refeição quando não saiu nenhum cliente do restaurante
+            if n_clientes_sairam > 0 or n_mesas_desocupadas > 0:
+                pagou = utils.ler_numero_decimal_limites(0,None,'Quanto custou a refeição:')
+                total_pago = total_pago + pagou
         if op == 3:
-            Estado(n_max_mesas,n_max_clientes,n_atual_mesas,n_atual_clientes)
+            Estado(n_max_mesas,n_max_clientes,n_atual_mesas,n_atual_clientes,total_pago)
 
 
 def Main():
